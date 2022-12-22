e86c6ce7ce543eb977aa578e99504e3df5ee17ce
antran1245 <antran1245@gmail.com>
2022-12-22 11:48:09 -0800

    basic game setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{tsx, ts}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      background: '#3B4262',
+      circle: '#BABFD4',
+      white: '#FFFFFF',
+      gold: '#EB9F0E',
+      darkGold: '#C76C1B',
+      red: '#DB2E4D',
+      darkRed: '#9D1634',
+      blue: '#4664F4',
+      darkBlue: '#2A45C2'
+    },
     extend: {},
   },
   plugins: [],
